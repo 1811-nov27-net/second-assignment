3fb6adec3ec363132ea9144f28b6e67753cb74b1
unknown <beauhansen45@gmail.com>
2018-12-03 11:51:02 -0600

    fixed bug related to using wrong name

--- WeekTwoAssessment/WeekTwoAssessment.Library/Word.cs
+++ WeekTwoAssessment/WeekTwoAssessment.Library/Word.cs
@@ -2,11 +2,56 @@
 
 namespace WeekTwoAssessment.Library
 {
-    class Program
+    public class Word
     {
-        static void Main(string[] args)
+        // private initialization of the name of the word that we are going to check
+        private string _name = "";
+
+        // constructor. If no parameters are passed, then the name of the word that we are going to check 
+        // should be the empty string
+        public Word()
         {
-            Console.WriteLine("Hello World!");
+            name = "";
+        }
+
+        // constructor. If the name of the word to check is passed, then
+        // that argument should take be automatically assigned.
+        public Word(string Name)
+        {
+            name = Name;
+        }
+        public string name
+        {
+            get 
+            { 
+                return _name;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        bool checkPalindrome(string _name)
+        {
+            int i = 0;
+            int j = _name.Length - 1;
+            bool isPalindrome = true;
+            while (i < j)
+            {
+                char a = _name[i];
+                char b = _name[j];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    isPalindrome = false;
+                }
+                i++;
+                j--;
+            }
+            return isPalindrome;
         }
     }
 }
