df99566289f0997d0f4eabe6e835bbea161db846
Howard Tran <howardlt94@gmail.com>
2018-12-03 11:50:19 -0600

    adding

--- Pizza.test/UnitTest1.cs
+++ Pizza.test/UnitTest1.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
-namespace Pizza.test
+
+namespace test.test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
 
-        }
+        
+        
+
+
     }
 }
--- Project.UI/Program.cs
+++ Project.UI/Program.cs
@@ -6,7 +6,19 @@ namespace Project.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] array =
+            {
+                "nurses run",
+                "racecaR",
+                "1221",
+                "never odd, or even.",
+                "one two one",
+                "123abccba123"
+            };
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome (value)); 
+            }
         }
     }
 }
