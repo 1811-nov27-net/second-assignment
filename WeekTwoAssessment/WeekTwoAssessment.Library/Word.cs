820e96a2ca4aaef574880a52651a6878a13048a1
unknown <beauhansen45@gmail.com>
2018-12-03 11:26:11 -0600

    Wrote code for palindrome and started first test

---
+++ WeekTwoAssessment/WeekTwoAssessment.Library/Word.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekTwoAssessment.Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WeekTwoAssessment/WeekTwoAssessment.Tests/WeekTwoAssessmentTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using WeekTwoAssessment.Library;
+
+
+namespace WeekTwoAssessment
+{
+    public class WeekTwoAssessmentTests
+    {
+        static void InstanceIsNotEmpty
+        {
+            // arrange
+            word sut = new IsPalindrome();
+
+            // act
+            result = sut.
+
+            // assert
+        }
+    }
+}
