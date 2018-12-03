2ab5df63425775945679c6adb9ba7e2fb1338340
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-03 11:26:54 -0600

    Created tester

---
+++ PalendromeTester/PalendromeTester/PalendromeTester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PalendromeTester
+{
+    public class PalendromeTester
+    {
+        public bool isPalendrome(string input)
+        {
+            string testString = trimInput(input);
+            for (int i = 0; i < testString.Length/2; i++)
+            {
+                if (testString[i] != testString[testString.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        private string trimInput(string input)
+        {
+            string ret = "";
+            input = input.ToLower();
+            for (int i=0; i < input.Length; i++)
+            {
+                if((input[i]>='A'&& input[i]<='Z') || (input[i]>='a' && input[i] <= 'z'))
+                {
+                    ret += input[i];
+                }
+            }
+            return ret;
+        }
+    }
+}
